4eee937ef7b2cfaeff5f6443f5922dcbcf56c8a2
CountrySideEngineer <deep-sleeper@nifty.com>
2019-10-20 16:20:21 +0900

    1st commit.

---
+++ MFC_BitOverlapSample/MFC_BitOverlapSample/MFC_BitOverlapSample.h
@@ -0,0 +1,32 @@
+﻿
+// MFC_BitOverlapSample.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMFCBitOverlapSampleApp:
+// このクラスの実装については、MFC_BitOverlapSample.cpp を参照してください
+//
+
+class CMFCBitOverlapSampleApp : public CWinApp
+{
+public:
+	CMFCBitOverlapSampleApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCBitOverlapSampleApp theApp;
---
+++ MFC_BitOverlapSample/MFC_BitOverlapSample/MFC_BitOverlapSampleDlg.cpp
@@ -0,0 +1,95 @@
+﻿
+// MFC_BitOverlapSampleDlg.cpp : 実装ファイル
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFC_BitOverlapSample.h"
+#include "MFC_BitOverlapSampleDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCBitOverlapSampleDlg ダイアログ
+
+
+
+CMFCBitOverlapSampleDlg::CMFCBitOverlapSampleDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFC_BITOVERLAPSAMPLE_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCBitOverlapSampleDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CMFCBitOverlapSampleDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_RUN_CHECK, &CMFCBitOverlapSampleDlg::OnBnClickedButtonRunCheck)
+END_MESSAGE_MAP()
+
+
+// CMFCBitOverlapSampleDlg メッセージ ハンドラー
+
+BOOL CMFCBitOverlapSampleDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+	//  Framework は、この設定を自動的に行います。
+	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+
+	// TODO: 初期化をここに追加します。
+
+	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+}
+
+// ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
+//  下のコードが必要です。ドキュメント/ビュー モデルを使う MFC アプリケーションの場合、
+//  これは、Framework によって自動的に設定されます。
+
+void CMFCBitOverlapSampleDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 描画のデバイス コンテキスト
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// クライアントの四角形領域内の中央
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// アイコンの描画
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
+//  システムがこの関数を呼び出します。
+HCURSOR CMFCBitOverlapSampleDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCBitOverlapSampleDlg::OnBnClickedButtonRunCheck()
+{
+	AfxMessageBox(_T("ボタンが押下されました。"), MB_OK);
+}
